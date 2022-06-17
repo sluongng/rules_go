c9320d0d59602cf19a41aa5ddbcf4cf4073cf7e9
Son Luong Ngoc <sluongng@gmail.com>
2022-06-17 10:28:17 +0200

    runfiles: port phst/runfiles to rules_go
    Today rules_go provide //go/tools/bazel as the canonical runfiles
    library for binaries to be executed with `bazel test` and `bazel run`.
    
    However, the current implementation pre-date the recent changes in
    Bazel's upstream.  Since then, all of the native runfiles library of
    Bash, Java, CPP, Python have been refactored to follow a certain
    convention in locating files. (1)
    
    Although these are subjected to change with the incoming BzlMod feature,
    it would be easier to maintain if we can keep rules_go's runfiles
    library implementation aligned with native languages' implementation.
    
    Today, it seems like https://github.com/phst/runfiles implemented
    exactly that.  So with @fmeum suggestion and @phst permission (2), let's
    port the newer, more accurate implementation to rules_go.
    
    Future refactoring will mark the current exported APIs in
    //go/tools/bazel as deprecated and/or swapping out the old implementation
    underneath to use this newer package.
    
    Changes in this PR included:
    - Copy paste repository over
    - Removal of .git and .gitignore and .githooks dir
    - Removal of repository specific files: README, WORKSPACE
    - Rename BUILD to BUILD.bazel
    - Rename import path for both go and BUILD files
    - Run gazelle over the packages
    - Adjusted test cases to reflect new package paths
    - Removed godoc related to installation instruction
    - Fixed test to handle window path separator
    
    (1): https://docs.google.com/document/d/e/2PACX-1vSDIrFnFvEYhKsCMdGdD40wZRBX3m3aZ5HhVj4CtHPmiXKDCxioTUbYsDydjKtFDAzER5eg7OjJWs3V/pub
    (2): https://github.com/phst/runfiles/issues/3#issuecomment-1003070026

---
+++ go/tools/bazel/runfiles/fs_test.go
@@ -0,0 +1,96 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build go1.16
+// +build go1.16
+
+package runfiles_test
+
+import (
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+	"testing/fstest"
+
+	"github.com/bazelbuild/rules_go/go/tools/bazel/runfiles"
+)
+
+func TestFS(t *testing.T) {
+	fsys, err := runfiles.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Ensure that the Runfiles object implements FS interfaces.
+	var _ fs.FS = fsys
+	var _ fs.StatFS = fsys
+	var _ fs.ReadFileFS = fsys
+	if err := fstest.TestFS(fsys, "io_bazel_rules_go/go/tools/bazel/runfiles/test.txt", "io_bazel_rules_go/go/tools/bazel/runfiles/testprog/testprog"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestFS_empty(t *testing.T) {
+	dir := t.TempDir()
+	manifest := filepath.Join(dir, "manifest")
+	if err := os.WriteFile(manifest, []byte("__init__.py \n"), 0600); err != nil {
+		t.Fatal(err)
+	}
+	fsys, err := runfiles.New(runfiles.ManifestFile(manifest), runfiles.ProgramName("/invalid"), runfiles.Directory("/invalid"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Run("Open", func(t *testing.T) {
+		fd, err := fsys.Open("__init__.py")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer fd.Close()
+		got, err := io.ReadAll(fd)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(got) != 0 {
+			t.Errorf("got nonempty contents: %q", got)
+		}
+	})
+	t.Run("Stat", func(t *testing.T) {
+		got, err := fsys.Stat("__init__.py")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if got.Name() != "__init__.py" {
+			t.Errorf("Name: got %q, want %q", got.Name(), "__init__.py")
+		}
+		if got.Size() != 0 {
+			t.Errorf("Size: got %d, want %d", got.Size(), 0)
+		}
+		if !got.Mode().IsRegular() {
+			t.Errorf("IsRegular: got %v, want %v", got.Mode().IsRegular(), true)
+		}
+		if got.IsDir() {
+			t.Errorf("IsDir: got %v, want %v", got.IsDir(), false)
+		}
+	})
+	t.Run("ReadFile", func(t *testing.T) {
+		got, err := fsys.ReadFile("__init__.py")
+		if err != nil {
+			t.Error(err)
+		}
+		if len(got) != 0 {
+			t.Errorf("got nonempty contents: %q", got)
+		}
+	})
+}
